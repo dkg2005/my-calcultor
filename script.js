1c9d6ed639efc34e1b45ab118dcaefe78b0262ec
dkg2005 <dhruvgupta766671@gmail.com>
2025-07-01 07:24:23 +0530

    Material Added

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(val){
+    if(display.innerText === '0'){
+        display.innerText = val;
+    }
+    else display.innerText += val;
+}
+
+function calculate(){
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (error) {
+        display.innerText = 'Error';
+    }
+}
+
+function clearDisplay(){
+    display.innerText = '0';
+}
+
+function deleteLast(){
+    if(display.innerText.length > 1){
+        display.innerText = display.innerText.slice(0, display.innerText.length - 1);   // for deleting last character we can also use slice(0, -1)
+    } else {
+        display.innerText = '0';
+    }
+}
