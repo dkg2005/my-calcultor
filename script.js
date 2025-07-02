9105fa28e31600551be3c2de5609792f2d6e2a6c
dkg2005 <dhruvgupta766671@gmail.com>
2025-07-02 20:51:44 +0530

    'Error' expression is fixed and size of display of calculator fixed

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ function appendToDisplay(val){
         display.innerText = val;
     }
     else display.innerText += val;
+    adjustFontSize();
 }
 
 function calculate(){
     try {
         display.innerText = eval(display.innerText);
     } catch (error) {
-        display.innerText = 'Error';
+        alert('Invalid expression');
+        clearDisplay();
     }
+    adjustFontSize();
 }
 
 function clearDisplay(){
     display.innerText = '0';
+    adjustFontSize();
 }
 
 function deleteLast(){
@@ -25,4 +29,21 @@ function deleteLast(){
     } else {
         display.innerText = '0';
     }
+    adjustFontSize();
 }
+
+function adjustFontSize() {
+    const length = display.innerText.length;
+
+    if (length <= 13) {
+        display.style.fontSize = '1.7rem';
+    } else if (length <= 18) {
+        display.style.fontSize = '1.3rem';
+    } else if (length > 15) {
+        alert('Expression too long, resetting to 0');
+        display.innerText = '0'; 
+        display.style.fontSize = '1.2rem';
+    }
+}
+
+adjustFontSize();
